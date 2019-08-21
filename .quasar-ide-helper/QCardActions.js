40f281f8b7b0960e350c17eb2a576fd45a3047ee
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2019-08-21 10:26:34 +0300

    ide-helper

---
+++ .quasar-ide-helper/QCardActions.js
@@ -0,0 +1,67 @@
+
+/**
+ * Quasar QCardActions component
+ * 
+ * @see {@link https://v1.quasar-framework.org/vue-components/card-actions|Quasar Docs} (Generated link, may not always work)
+ */  
+export default {
+  name: 'QCardActions',
+  props: {
+    /**
+     * Specify how to align the actions
+     * @type {'left'|'center'|'right'|'between'|'around'|'stretch'}
+     */
+    align: {
+      type: String,
+    },
+    /**
+     * Specify how to align the actions
+     * @type {'left'|'center'|'right'|'between'|'around'|'stretch'}
+     */
+    'align="left"': {
+      type: String,
+    },
+    /**
+     * Specify how to align the actions
+     * @type {'left'|'center'|'right'|'between'|'around'|'stretch'}
+     */
+    'align="center"': {
+      type: String,
+    },
+    /**
+     * Specify how to align the actions
+     * @type {'left'|'center'|'right'|'between'|'around'|'stretch'}
+     */
+    'align="right"': {
+      type: String,
+    },
+    /**
+     * Specify how to align the actions
+     * @type {'left'|'center'|'right'|'between'|'around'|'stretch'}
+     */
+    'align="between"': {
+      type: String,
+    },
+    /**
+     * Specify how to align the actions
+     * @type {'left'|'center'|'right'|'between'|'around'|'stretch'}
+     */
+    'align="around"': {
+      type: String,
+    },
+    /**
+     * Specify how to align the actions
+     * @type {'left'|'center'|'right'|'between'|'around'|'stretch'}
+     */
+    'align="stretch"': {
+      type: String,
+    },
+    /**
+     * Display actions one below the other
+     * @type {Boolean}
+     */
+    vertical: {
+      type: Boolean,
+    }
+  }
+}
