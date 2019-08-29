4443cebdf0d633e9c7555c3e46216b6f5fc8a9d0
LeoAndLis <lis.and.leo@gmail.com>
2019-08-29 15:25:35 +0300

    Change Catalog.vue

---
+++ src/components/admin/AdminTree.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-tree
+      :nodes="simple"
+      node-key="label"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminTree',
+  props: ['simple'],
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/pages/admin/Catalog.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-page class="flex justify-center">
+    <AdminTree></AdminTree>
+  </q-page>
+</template>
+
+<script>
+import AdminTree from '../../components/admin/AdminTree';
+
+export default {
+  name: 'Products',
+  components: { AdminTree },
+  data() {
+    return {
+      simple: [
+        {
+          label: 'Satisfied customers (with avatar)',
+          avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
+          children: [
+            {
+              label: 'Good food (with icon)',
+              icon: 'restaurant_menu',
+              children: [
+                { label: 'Quality ingredients' },
+                { label: 'Good recipe' },
+              ],
+            },
+            {
+              label: 'Good service (disabled node with icon)',
+              icon: 'room_service',
+              disabled: true,
+              children: [
+                { label: 'Prompt attention' },
+                { label: 'Professional waiter' },
+              ],
+            },
+            {
+              label: 'Pleasant surroundings (with icon)',
+              icon: 'photo',
+              children: [
+                {
+                  label: 'Happy atmosphere (with image)',
+                  img: 'https://cdn.quasar.dev/img/logo_calendar_128px.png',
+                },
+                { label: 'Good table presentation' },
+                { label: 'Pleasing decor' },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
