e8a7055a75a2b798ee1872d97dd3a0bed66cd2e1
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2019-12-07 22:44:46 +0300

    Анимация скрытия и открытия фильтров

--- src/components/catalog/CatalogLeftMenuFilter.vue
+++ src/components/catalog/CatalogLeftMenuFilter.vue
@@ -12,51 +12,66 @@
           <div class="bx_filter_parameters_box active title">
             <div class="bx_filter_parameters_box_title">Фильтр по параметрам</div>
           </div>
-          <div class="bx_filter_parameters_box active">
-            <div class="bx_filter_parameters_box_title icons_fa">Цена</div>
-            <div class="bx_filter_block">
-              <div class="bx_filter_parameters_box_container numbers">
-                <div class="wrapp_all_inputs wrap_md">
-                  <div class="wrapp_change_inputs iblock">
-                    <div class="bx_filter_parameters_box_container_block">
-                      <div class="bx_filter_input_container form-control bg">
-                        <input
-                          class="min-price"
-                          type="text"
-                          v-model="rangeSlider.min"
-                         >
+
+          <div
+            class="bx_filter_parameters_box">
+            <div class="bx_filter_parameters_box_title icons_fa"
+             @click="onClickFilterItem('price')"
+            >
+              Цена
+              <q-icon
+                class="catalog-left-menu-filter__bx-filter-label-icon"
+                :name="activeFilterItems.price ? 'expand_less' : 'expand_more'"
+              />
+          </div>
+          <div
+            v-bind:class="{ active: activeFilterItems.price }"
+            class="catalog-left-menu-filter__bx-filter-block bx_filter_block"
+          >
+            <div class="bx_filter_parameters_box_container numbers">
+                  <div class="wrapp_all_inputs wrap_md">
+                    <div class="wrapp_change_inputs iblock">
+                      <div class="bx_filter_parameters_box_container_block">
+                        <div class="bx_filter_input_container form-control bg">
+                          <input
+                            class="min-price"
+                            type="text"
+                            v-model="rangeSlider.min"
+                           >
+                        </div>
                       </div>
-                    </div>
-                    <div class="bx_filter_parameters_box_container_block">
-                      <div class="bx_filter_input_container form-control bg">
-                        <input
-                          class="max-price"
-                          type="text"
-                          size="5"
-                          v-model="rangeSlider.max"
-                        >
+                      <div class="bx_filter_parameters_box_container_block">
+                        <div class="bx_filter_input_container form-control bg">
+                          <input
+                            class="max-price"
+                            type="text"
+                            size="5"
+                            v-model="rangeSlider.max"
+                          >
+                        </div>
                       </div>
+                      <div style="clear: both;"></div>
                     </div>
                     <div style="clear: both;"></div>
-                  </div>
-                  <div style="clear: both;"></div>
-                  <div class="wrapp_slider iblock">
-                    <div
-                       class="flex justify-between"
-                       style="margin-bottom: -17px;margin-top: 5px;">
-                      <span style="color:#999;">{{minPrice}}</span>
-                      <span style="color:#999;">{{maxPrice}}</span>
+                    <div class="wrapp_slider iblock">
+                      <div
+                         class="flex justify-between"
+                         style="margin-bottom: -17px;margin-top: 5px;">
+                        <span style="color:#999;">{{minPrice}}</span>
+                        <span style="color:#999;">{{maxPrice}}</span>
+                      </div>
+                      <div class="catalog-left-menu-filte__range-slider">
+                        <q-range
+                          v-model="rangeSlider"
+                          :min="minPrice"
+                          :max="maxPrice"
+                          color="main-brown"
+                        />
+                      </div>
                     </div>
-                    <q-range
-                            v-model="rangeSlider"
-                            :min="minPrice"
-                            :max="maxPrice"
-                            color="main-brown"
-                    />
                   </div>
                 </div>
-              </div>
-            </div>
+          </div>
           </div>
           <div class="clb"></div>
           <div class="bx_filter_button_box active">
@@ -98,6 +113,9 @@ export default {
   data() {
     return {
       minPrice: 0,
+      activeFilterItems: {
+        price: false,
+      },
       rangeSlider: {
         min: 0,
         max: 0,
@@ -114,7 +132,9 @@ export default {
     },
     onClickReset() {
       this.resetFilter({ minPrice: 0, maxPrice: null });
-      // this.filter({ minPrice: 0, maxPrice: null });
+    },
+    onClickFilterItem(item) {
+      this.activeFilterItems[item] = !this.activeFilterItems[item];
     },
   },
 };
@@ -126,5 +146,21 @@ export default {
   }
   .bg-main-brown {
     background: #512c1d;
+  }
+  .catalog-left-menu-filter__bx-filter-block {
+    max-height: 0;
+    transition: max-height .4s;
+    overflow: hidden;
+  }
+  .catalog-left-menu-filter__bx-filter-block.active{
+    max-height: 100px;
+  }
+  .catalog-left-menu-filte__range-slider {
+    margin: 0 11px;
+  }
+  .catalog-left-menu-filter__bx-filter-label-icon {
+    position: absolute;
+    right: -8px;
+    top: 4px;
   }
 </style>
